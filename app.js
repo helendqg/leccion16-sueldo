8b074e2e06880ebf7309bcea3ee13f81bbbf8cf3
helen <hqgamboa@gmail.com>
2016-08-25 17:44:14 -0500

    sueldo

---
+++ app.js
@@ -0,0 +1,7 @@
+var horasTrabajadas = prompt("Ingrese horas trabajadas en la semana");
+    horasTrabajadas = parseInt(horasTrabajadas);
+var pagoPorHora = prompt("Ingrese pago por hora en s/.");
+    pagoPorHora = parseInt(pagoPorHora);
+var sueldoSemanal = horasTrabajadas*pagoPorHora;
+document.write("su sueldo semanal es: s/.");
+document.write(sueldoSemanal);
